01048561d623f7e07bb1ef18d205bcd3da21c5f2
Volodumur Besarab <volodumur911@gmail.com>
2023-09-26 17:42:11 +0300

    add files

---
+++ modules/Otomoto/otomoto_manager.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from pandas import DataFrame
+
+
+class OtomotoManager:
+    def __init__(self, exel_file_page: dict[Any, DataFrame]):
+        self.exel_file_page = exel_file_page
+
+    def read_page_line(self):
+        status = "Error"
+        # read article count in storage
+            # read article id
+                # create product
+                # read other atribbutes
+                    #edit atributes
+
+        status = "Complete"
+        return status
+
+    def read_all_items(self):
+        pass

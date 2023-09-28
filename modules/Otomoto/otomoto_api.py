4bd82621011049e52737d208b21a884ad0714ee3
Volodumur Besarab <volodumur911@gmail.com>
2023-09-28 22:04:16 +0300

    advert ready to create

--- modules/Otomoto/otomoto_api.py
+++ modules/Otomoto/otomoto_api.py
@@ -2,133 +2,174 @@ import json
 import requests
 from modules.auth_manager import AuthManager
 
-class OtomotoApi:
 
+class OtomotoApi:
     def __init__(self):
         self.auth_manager = AuthManager()
+        self.access_token = None
 
-    def start(self):
-        otomoto_url = self.auth_manager.get_otomoto_url()
-        otomoto_client_id = self.auth_manager.get_otomoto_client_id()
-        otomoto_client_secret = self.auth_manager.get_otomoto_client_secret()
-        otomoto_username = self.auth_manager.get_otomoto_username()
-        otomoto_password = self.auth_manager.get_otomoto_password()
+    def get_token(self):
+        if not self.access_token:
+            otomoto_url = self.auth_manager.get_otomoto_url()
+            otomoto_client_id = self.auth_manager.get_otomoto_client_id()
+            otomoto_client_secret = self.auth_manager.get_otomoto_client_secret()
+            otomoto_username = self.auth_manager.get_otomoto_username()
+            otomoto_password = self.auth_manager.get_otomoto_password()
 
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
 
-        start_data = {
-            "grant_type": "password",
-            "username": otomoto_username,
-            "password": otomoto_password
-        }
+            start_data = {
+                "grant_type": "password",
+                "username": otomoto_username,
+                "password": otomoto_password
+            }
 
-        response = requests.post(otomoto_url, data=start_data, headers=headers, auth=(otomoto_client_id, otomoto_client_secret))
-        access_token = None
-        if response.status_code == 200:
-            access_token = response.json().get("access_token")
-            # remove
-            print("Access Token:", access_token)
-        else:
-            print("Error:", response.status_code, response.text)
-
+            response = requests.post(otomoto_url, data=start_data, headers=headers, auth=(otomoto_client_id, otomoto_client_secret))
+            if response.status_code == 200:
+                access_token = response.json().get("access_token")
+                print("New access token was acquired from OtoMoto")
+            else:
+                print("Error:", response.status_code, response.text)
+            return self.access_token
+        return self.access_token
         # otomoto api виведення в консоль даних про оголошення
 
-        url = "https://www.otomoto.pl/api/open/account/adverts"
+    def _get_basic_headers(self, access_token):
         user_email = "andrewb200590@gmail.com"
-        limit = 10
-        page = 1
-
         headers = {
             "User-Agent": user_email,
             "Content-Type": "application/json",
             "Authorization": f"Bearer {access_token}"
         }
+        return headers
+
+    def _get_basic_url(self):
+        url = "https://www.otomoto.pl/api/open/account/adverts"
+        return url
+
+    def print_adverts_list(self, access_token):
+        url = self._get_basic_url()
+        limit = 10
+        page = 1
+
+        headers = self._get_basic_headers(access_token)
 
         params = {
             "limit": limit,
             "page": page
         }
 
-        def print_adverts_list():
-            response = requests.get(url, headers=headers, params=params)
+        response = requests.get(url, headers=headers, params=params)
 
-            if response.status_code == 200:
-                adverts_data = response.json()
-                # Process the adverts_data as needed
-                print("Adverts data:", adverts_data)
-            else:
-                print("Error:", response.status_code, response.text)
+        if response.status_code == 200:
+            adverts_data = response.json()
+            # Process the adverts_data as needed
+            print("Adverts data:", adverts_data)
+        else:
+            print("Error:", response.status_code, response.text)
 
-        print_adverts_list()
 
-        # створення колекції з фото (код не тестований, вже як матимемо фото варто спробувати + дописати рядок до data "image_collection_id": 821165355"
-        # def create_image_collection(user_email, access_token, image_urls):
-        #     url = "https://www.otomoto.pl/api/open/imageCollections"
-        #
-        #     headers = {
-        #         "User-Agent": user_email,
-        #         "Content-Type": "application/json",
-        #         "Authorization": f"Bearer {access_token}"
-        #     }
-        #
-        #     data = image_urls
-        #
-        #     response = requests.post(url, json=data, headers=headers)
-        #
-        #     if response.status_code == 200:
-        #         collection_id = response.json().get("id")
-        #         print(f"Image collection successfully created with ID: {collection_id}")
-        #         return collection_id
-        #     else:
-        #         print("Error:", response.status_code, response.text)
-        #         return None
-        #
-        # # Replace these with your actual values
-        # user_email = "your_user_email"
-        # access_token = "your_access_token"
-        #
-        # image_urls = {
-        #     "1": "http://lorempixel.com/800/600/transport/",
-        #     "2": "http://lorempixel.com/800/600/transport/",
-        #     "3": "http://lorempixel.com/800/600/transport/"
-        # }
 
-        # створення нового оголошення otomoto
+    # створення колекції з фото (код не тестований, вже як матимемо фото варто спробувати + дописати рядок до data "image_collection_id": 821165355"
+    # def create_image_collection(user_email, access_token, image_urls):
+    #     url = "https://www.otomoto.pl/api/open/imageCollections"
+    #
+    #     headers = {
+    #         "User-Agent": user_email,
+    #         "Content-Type": "application/json",
+    #         "Authorization": f"Bearer {access_token}"
+    #     }
+    #
+    #     data = image_urls
+    #
+    #     response = requests.post(url, json=data, headers=headers)
+    #
+    #     if response.status_code == 200:
+    #         collection_id = response.json().get("id")
+    #         print(f"Image collection successfully created with ID: {collection_id}")
+    #         return collection_id
+    #     else:
+    #         print("Error:", response.status_code, response.text)
+    #         return None
+    #
+    # # Replace these with your actual values
+    # user_email = "your_user_email"
+    # access_token = "your_access_token"
+    #
+    # image_urls = {
+    #     "1": "http://lorempixel.com/800/600/transport/",
+    #     "2": "http://lorempixel.com/800/600/transport/",
+    #     "3": "http://lorempixel.com/800/600/transport/"
+    # }
 
-        def activate_advert(advert_id, user_email, access_token):
-            url = f"https://www.otomoto.pl/api/open/account/adverts/{advert_id}/activate"
-
-            response = requests.post(url, headers=headers)
-
-            if response.status_code == 200:
-                print(f"Advert with ID {advert_id} successfully activated!")
-            else:
-                print("Error:", response.status_code, response.text)
-
-            # def post_advert(user_email, access_token):
-            #     url = "https://www.otomoto.pl/api/open/account/adverts"
-            #
-            #     headers = {
-            #         "User-Agent": user_email,
-            #         "Content-Type": "application/json",
-            #         "Authorization": f"Bearer {access_token}"
-            #     }
-
-            # Винести статичні данні і поміняти
-            #CITY = "Lublin"
+    # створення нового оголошення otomoto
 
         # Зчитуєм данні з таблиці
         # Зберігаєм данні в словнику
         # Передаєм словник генератору дати
         # В даті розпаковуєм і створюєм
 
-        # треба поміняти тут код на змінні які будемо отримувати з таблиці
-        custom_otomoto_data = {
+    # треба поміняти тут код на змінні які будемо отримувати з таблиці
+    custom_otomoto_data = {
+        "status": "unpaid",
+        'title': 'Poprzeczki Belki bagaznik Thule Aerobar 3',
+        'description': 'Poprzeczki Thule Aerobar\n108cm-200zł\n120cm-230zł\n127 cm-270zł\n135cm-300zł\n150cm- 350zł\nDostępność proszę sprawdz.\nWystawiam Fakturę VAT marża.\nWysyłka kurierem za pobraniem.',
+        'category_id': 173,
+        'region_id': 8,
+        'city_id': 10119,
+        'municipality': 'Lublin',
+        'city': {
+            'pl': 'Lublin',
+            'en': 'Lublin'
+        },
+        'coordinates': {
+            'latitude': 51.23955,
+            'longitude': 22.55257,
+            'radius': 0,
+            'zoom_level': 13
+        },
+        'advertiser_type': 'business',
+        'contact': {
+            'person': 'Andrzej Besarab',
+            'phone_numbers': [
+                '+48660086570'
+            ]
+        },
+        "params": {
+            'tire-brand': 'others',
+            'delivery': '1',
+            'title_parts': 'Poprzeczki Belki bagaznik Thule Aerobar delux',
+            'manufacturer': 'Thule',
+            "price": {
+                '0': 'price',
+                '1': 200,
+                'currency': 'PLN',
+                'gross_net': 'gross'
+            },
+        },
+        'new_used': 'used',
+        'visible_in_profile': '1',
+    }
+
+    def _exel_info_dict_creator(self, title, description, price, new_used, manufacturer):
+        exel_info_dict = {
+            "title": title,
+            "description": description,
+            "price": price,
+            "new_used": new_used,
+            "manufacturer": manufacturer,
+        }
+        return exel_info_dict
+
+    def _data_creator(self, exel_info_dict) -> json:
+        if exel_info_dict is None or exel_info_dict == {}:
+            print("Dictionaty is empty")
+        otomoto_data = {
             "status": "unpaid",
-            'title': 'Poprzeczki Belki bagaznik Thule Aerobar 3',
-            'description': 'Poprzeczki Thule Aerobar\n108cm-200zł\n120cm-230zł\n127 cm-270zł\n135cm-300zł\n150cm- 350zł\nDostępność proszę sprawdz.\nWystawiam Fakturę VAT marża.\nWysyłka kurierem za pobraniem.',
+            'title': exel_info_dict["title"],
+            'description': exel_info_dict["description"],
             'category_id': 173,
             'region_id': 8,
             'city_id': 10119,
@@ -153,94 +194,50 @@ class OtomotoApi:
             "params": {
                 'tire-brand': 'others',
                 'delivery': '1',
-                'title_parts': 'Poprzeczki Belki bagaznik Thule Aerobar delux',
-                'manufacturer': 'Thule',
+                'title_parts': exel_info_dict["title"],
+                'manufacturer': exel_info_dict["manufacturer"],
                 "price": {
                     '0': 'price',
-                    '1': 200,
+                    '1': exel_info_dict["price"],
                     'currency': 'PLN',
                     'gross_net': 'gross'
                 },
             },
-            'new_used': 'used',
+            'new_used': exel_info_dict["new_used"],
             'visible_in_profile': '1',
         }
+        return otomoto_data
 
-        def exel_info_dict_creator(title, description, price, new_used, manufacturer):
-            exel_info_dict = {
-                "title": title,
-                "description": description,
-                "price": price,
-                "new_used": new_used,
-                "manufacturer": manufacturer,
-            }
-            return exel_info_dict
+    def create_otomoto_advert(self, title, description, price, new_used, manufacturer):
+        exel_info_dict = self._exel_info_dict_creator(title=title,
+                                                      description=description,
+                                                      price=price,
+                                                      new_used=new_used,
+                                                      manufacturer=manufacturer)
+        otomoto_data = self._data_creator(exel_info_dict=exel_info_dict)
+        url = self._get_basic_url()
+        access_token = self.get_token()
+        headers = self._get_basic_headers(access_token=access_token)
+        response = requests.post(url, json=otomoto_data, headers=headers)
 
-        def data_creator(exel_info_dict) -> json:
-            if dict == None or dict == {}:
-                print("Dictionaty is empty")
-            otomoto_data = {
-                "status": "unpaid",
-                'title': exel_info_dict["title"],
-                'description': exel_info_dict["description"],
-                'category_id': 173,
-                'region_id': 8,
-                'city_id': 10119,
-                'municipality': 'Lublin',
-                'city': {
-                    'pl': 'Lublin',
-                    'en': 'Lublin'
-                },
-                'coordinates': {
-                    'latitude': 51.23955,
-                    'longitude': 22.55257,
-                    'radius': 0,
-                    'zoom_level': 13
-                },
-                'advertiser_type': 'business',
-                'contact': {
-                    'person': 'Andrzej Besarab',
-                    'phone_numbers': [
-                        '+48660086570'
-                    ]
-                },
-                "params": {
-                    'tire-brand': 'others',
-                    'delivery': '1',
-                    'title_parts': exel_info_dict["title"],
-                    'manufacturer': exel_info_dict["manufacturer"],
-                    "price": {
-                        '0': 'price',
-                        '1': exel_info_dict["price"],
-                        'currency': 'PLN',
-                        'gross_net': 'gross'
-                    },
-                },
-                'new_used': exel_info_dict["new_used"],
-                'visible_in_profile': '1',
-            }
-            return otomoto_data
-
-        def create_otomoto_advert(otomoto_data):
-            response = requests.post(url, json=otomoto_data, headers=headers)
-
-            if response.status_code == 201:
-                advert_id = response.json().get("id")
-                print(f"Advert successfully posted with ID: {advert_id}")
-                return advert_id
-            else:
-                print("Error:", response.status_code, response.text)
-                return None
+        if response.status_code == 201:
+            advert_id = response.json().get("id")
+            print(f"Advert successfully posted with ID: {advert_id}")
+            return advert_id
+        else:
+            print("Error:", response.status_code, response.text)
+            return None
 
         # Replace these with your actual values
         # user_email = "andrewb200590@gmail.com"
         #
         # advert_id = post_advert(user_email, access_token)
+    def api_test(self):
         my_30_char_description = "qwertyuio qwertyuio qwertyuio zaoza"
-        first_item = exel_info_dict_creator(title="Title 1", description=my_30_char_description, price=101, new_used="used",
+        first_item = self.exel_info_dict_creator(title="Title 1", description=my_30_char_description, price=101, new_used="used",
                                             manufacturer="Manufacturer 1")
-        first_item_dict = data_creator(first_item)
-        advert_id = create_otomoto_advert(first_item_dict)
+        first_item_dict = self.data_creator(first_item)
+        advert_id = self.create_otomoto_advert(first_item_dict)
 
         # collection_id = create_image_collection(user_email, access_token, image_urls)
 
--- modules/Otomoto/otomoto_manager.py
+++ modules/Otomoto/otomoto_manager.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 from pandas import DataFrame
 
+from modules.Otomoto.otomoto_api import OtomotoApi
 from modules.excel_handler import ExcelHandler
 
 def read_page_line():
@@ -24,12 +25,13 @@ class OtomotoManager:
         self.file_name = file_name
         self.sheet_name = sheet_name
         self.excel_handler = ExcelHandler()
+        self.otomoto_api = OtomotoApi()
 
     def create_lists_of_produts(self, df1) -> tuple[list[DataFrame], list[DataFrame], list[DataFrame]]:
         in_stock = []
         out_of_stock = []
         invalid_quantity = []
-        for index, row in df1.head(100).iterrows():
+        for index, row in df1.iterrows():
             if row['наявність на складі'] < 0:
                 invalid_quantity.append(row)
             elif row['наявність на складі'] == 0:
@@ -37,6 +39,41 @@ class OtomotoManager:
             elif row['наявність на складі'] > 0:
                 in_stock.append(row)
         return in_stock, out_of_stock, invalid_quantity
+
+    def _convert_adverts_to_dict(self, list_ready_to_create: list[DataFrame]) -> list[dict]:
+        advert_dict = {}
+        list_of_adverts_dict = []
+        for row in list_ready_to_create:
+            for column_name, value in row.items():
+                advert_dict.update({column_name: value})
+            # print(advert_dict)
+            list_of_adverts_dict.append(advert_dict)
+            advert_dict = {}
+        return list_of_adverts_dict
+
+    def post_adverts(self, list_ready_to_create: list[DataFrame]):
+        testttt = self._convert_adverts_to_dict(list_ready_to_create)
+        print(testttt)
+        # advert_dict = {}
+        # for row in list_ready_to_create:
+        #     for column_name, value in row.items():
+        #         advert_dict.update({column_name: value})
+        #     print(advert_dict)
+        #     advert_dict = {}
+                # print(f"{column_name}: {value}")
+        # self.otomoto_api.create_otomoto_advert()
+
+    def create_list_need_to_create(self, in_stock: list[DataFrame]) -> tuple[list[DataFrame], list[DataFrame]]:
+        list_check_need_to_edit = []  # Ліст для товарів з непорожнім полем "ID otomoto"
+        list_ready_to_create = []  # Ліст для товарів з порожнім полем "ID otomoto"
+
+        for item in in_stock:
+            if not pd.isna(item['ID otomoto']):  # Перевірка, чи поле "ID otomoto" не порожнє
+                list_check_need_to_edit.append(item)
+            else:
+                list_ready_to_create.append(item)
+
+        return list_check_need_to_edit, list_ready_to_create
 
     def read_page_line(self):
         status = "Error"
@@ -64,10 +101,18 @@ class OtomotoManager:
         in_stock, out_of_stock, invalid_quantity = self.create_lists_of_produts(first_100_values)
         print(len(in_stock), len(out_of_stock), len(invalid_quantity))
 
+        # self.otomoto_api.
+
         columns_to_display = ["номер на складі", "наявність на складі", "title", "description", "price", "new_used"]
 
-        num_rows, num_columns = first_100_values.shape
-        print(num_rows, num_columns)
+        # num_rows, num_columns = first_100_values.shape
+        # print(num_rows, num_columns)
+
+        list_check_need_to_edit, list_ready_to_create = self.create_list_need_to_create(in_stock)
+        print("edit", len(list_check_need_to_edit))
+        print("create", len(list_ready_to_create))
+
+        self.post_adverts(list_ready_to_create)
 
 
 

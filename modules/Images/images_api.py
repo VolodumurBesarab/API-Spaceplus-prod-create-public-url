14f6e5f665e42d2bcfbb84819a0a7430a90d2db8
Volodumur Besarab <volodumur911@gmail.com>
2023-09-29 22:43:02 +0300

    images api done

--- modules/Images/images_api.py
+++ modules/Images/images_api.py
@@ -1,6 +1,6 @@
 import os
 
-from imgurpython import ImgurClient
+from Data.imgurpython import ImgurClient
 
 CLIENT_ID = 'f82bcb68847441a'
 CLIENT_SECRET = 'd3870caebcf64e7cc4ab0d180204d25b292dc3b6'
@@ -40,17 +40,19 @@ class ImagesApi:
         #     image_urls.append(image_url)
         #
         client = ImgurClient(CLIENT_ID, CLIENT_SECRET)
-
+        image_urls = []
         # Завантаження фото
         for image_name in image_files:
             final_path = os.path.join(folder_path, image_name)
-            print(final_path)
-            uploaded_image = client.upload_from_path(final_path, anon=True)
-
-            # Отримання URL завантаженого фото
-            image_url = uploaded_image['link']
-            print(image_url)
-        # return image_url
+            print("Download:", final_path)
+            try:
+                uploaded_image = client.upload_from_path(final_path, anon=True)
+                image_url = uploaded_image['link']
+                image_urls.append(image_url)
+                print("Success")
+            except Exception as e:
+                print(f"Помилка при завантаженні файлу {final_path}: {str(e)}")
+        return image_urls
 
     # Завантаження фото і отримання URL
     # imgur_image_url = upload_image_to_imgur(image_path)

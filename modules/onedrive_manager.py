4939dfb617aa77ff63ade66359a53cbdc203c458
Volodumur Besarab <volodumur911@gmail.com>
2023-11-01 17:32:17 +0200

    fix file naming

--- modules/onedrive_manager.py
+++ modules/onedrive_manager.py
@@ -15,13 +15,13 @@ class OneDriveManager:
             file_name = os.path.basename(file_path)
         else:
             base_name, extension = os.path.splitext(file_path)
-            file_name = f"{base_name} {rows_to_skip}-{rows_to_skip + rows_to_read}{extension}"
-
+            new_file_path = f"{base_name} {rows_to_skip}-{rows_to_skip + rows_to_read}{extension}"
+            file_name = os.path.basename(new_file_path)
             # file_name_without_extension = os.path.splitext(os.path.basename(file_path))[0]
             # file_extension = os.path.splitext(file_path)[1]
             # file_name = f"{file_name_without_extension} {rows_to_skip}-{rows_to_skip + rows_to_read}{file_extension}"
 
-        upload_url = self.auth_manager.get_endpoint() + f"drive/items/root:/Holland/{file_name}:/content"
+        upload_url = self.auth_manager.get_endpoint() + f"drive/items/root:/Holland/Reports/{file_name}:/content"
         access_token = self.access_token
         headers_octet_stream = {
             'Authorization': access_token,
@@ -31,7 +31,7 @@ class OneDriveManager:
         with open(file_path, 'rb') as upload:
             media_content = upload.read()
 
-        response = requests.put(url=upload_url, headers=headers_octet_stream, data=headers_octet_stream)
+        response = requests.put(url=upload_url, headers=headers_octet_stream, data=media_content)
         if response.status_code == 201 or response.status_code == 200:
             print("Файл успішно завантажено на OneDrive!")
         else:

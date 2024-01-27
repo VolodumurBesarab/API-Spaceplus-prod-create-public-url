9a16d02763ff624cbe81404d776612fc45f5bb03
VolodumurBesarab <volodumur911@gmail.com>
2024-01-27 17:53:19 +0300

    add reports generator

---
+++ modules/reports/reports_generator.py
@@ -0,0 +1,23 @@
+import os
+from modules.otomoto.otomoto_manager import DATETIME
+from modules.onedrive_manager import OneDriveManager
+
+REPORT_FILE_PATH = f"/tmp/reports/general_report {DATETIME}.txt"
+
+
+class ReportsGenerator:
+    def __init__(self):
+        self.one_drive_manager = OneDriveManager()
+
+    def create_general_report(self, message: str) -> str:
+        folder_path = "/tmp/reports"
+        file_path = REPORT_FILE_PATH
+
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        with open(file_path, "a") as file:
+            file.write(message + "\n")
+
+        self.one_drive_manager.upload_file_to_onedrive(file_path=REPORT_FILE_PATH)
+        return file_path
